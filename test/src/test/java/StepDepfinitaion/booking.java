7788850bdf5d79e72073c40164ac2d088c68e8d1
Ajwkh <Ajwkh>
2025-01-16 16:22:06 -0500

    cucumber table and scenario outline

--- test/src/test/java/StepDepfinitaion/booking.java
+++ test/src/test/java/StepDepfinitaion/booking.java
@@ -4,23 +4,18 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import io.cucumber.datatable.DataTable;
-
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
-
 public class booking {
     private WebDriver driver;
-
     @Before
     public void beforehock() {
         System.setProperty("webdriver.edge.driver", "C:\\Users\\18042\\msedgedriver.exe");
         driver = new EdgeDriver();
     }
-
     @After
     public void afterhock() {
         if (driver != null) {
@@ -38,35 +33,46 @@ public class booking {
     public void userEntersFlightDetails(DataTable dataTable) {
         List<String> genders = dataTable.asList(String.class);
         for (String gender : genders) {
-            WebElement pageTitle = driver.findElement(By.cssSelector("body > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > header:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div:nth-child(2)"));
-            String elemnettext = pageTitle.getText();
-            if (elemnettext.contains("Techlistic")) {
-                System.out.println("title is valid");
-            } else {
-                System.out.println("title is not validated");
-            }
-            
-            WebElement firstName=  driver.findElement(By.xpath("//input[@name='lastname']"));
-            firstName.clear();
-            firstName.sendKeys("Weyal");
-            System.out.println("name was typed");
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            WebElement lastName=  driver.findElement(By.xpath("//input[@name='lastname']"));
-            lastName.clear();
-            lastName.sendKeys("Weyal");
-            System.out.println("Last name was typed");
+            enterFlightDetails(gender);
+        }
+    }
 
-            List<WebElement> radios = driver.findElements(By.xpath("//input[@name='sex']"));
-            for (WebElement radio : radios) {
-                if (radio.getAttribute("value").equalsIgnoreCase(gender)) {
-                    radio.click();
-                    System.out.println(gender + " radio button is selected");
-                    break;
-                }
+    @When("User enters flight details {string}")
+    public void userEntersFlightDetails(String gender) {
+        enterFlightDetails(gender);
+    }
+
+    private void enterFlightDetails(String gender) {
+        WebElement pageTitle = driver.findElement(By.cssSelector("body > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > header:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div:nth-child(2)"));
+        String elemnettext = pageTitle.getText();
+        if (elemnettext.contains("Techlistic")) {
+            System.out.println("title is valid");
+        } else {
+            System.out.println("title is not validated");
+        }
+
+        WebElement firstName = driver.findElement(By.xpath("//input[@name='firstname']"));
+        firstName.clear();
+        firstName.sendKeys("Ajmal");
+        System.out.println("name was typed");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement lastName = driver.findElement(By.xpath("//input[@name='lastname']"));
+        lastName.clear();
+        lastName.sendKeys("weyal");
+        System.out.println("Last name was typed");
+
+        List<WebElement> radios = driver.findElements(By.xpath("//input[@name='sex']"));
+        for (WebElement radio : radios) {
+            if (radio.getAttribute("value").equalsIgnoreCase(gender)) {
+                radio.click();
+                System.out.println(gender + " radio button is selected");
+                break;
             }
         }
     }
--- test/src/test/resources/testfileoen.feature
+++ test/src/test/resources/testfileoen.feature
@@ -3,19 +3,18 @@ Feature: Flight Booking Functionality
 Scenario: Successful flight booking
     Given User is on the flight booking page
     When User enters flight details
-    |Female|
-    |Male|
+        | Female |
+        | Male   |
     And User proceeds to payment
     Then User receives a booking confirmation
 
-
-Scenario Outline: Successful flight booking
+Scenario Outline: Successful flight booking with different data
     Given User is on the flight booking page
-    When User enters flight details
+    When User enters flight details "<gender>"
     And User proceeds to payment
     Then User receives a booking confirmation
-    
-    Example
-    |radio|
-    |Female|
-    |Male|
+
+    Examples:
+    | gender |
+    | Female |
+    | Male   |
